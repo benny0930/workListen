a46fd49411f5cbdb00cefd6423090d401685a488
benny0930 <benny.kh.gy@gmail.com>
2021-03-12 15:45:21 +0800

    admin

--- workListenAction.php
+++ workListenAction.php
@@ -30,18 +30,25 @@ if ($type == "read") {
     echo $sDate;
 } else if ($type == "end") {
     $id = $_GET['id'];
+    $admin = $_GET['admin'];
     $broadcast = 'broadcast.txt';
     $str = file_get_contents($broadcast);
     $aDate = json_decode($str, true);
     $saveDate = Null;
     $aNewDate = [];
-    foreach ($aDate as $key => $oDate) {
-        if ($key > 0) {
-            $aNewDate[] = $oDate;
-        } else {
-            $saveDate = $oDate;
+    if($admin=="admin"){
+        foreach ($aDate as $key => $oDate) {
+            if ($key > 0) {
+                $aNewDate[] = $oDate;
+            } else {
+                $saveDate = $oDate;
+            }
         }
     }
+    else{
+        $aNewDate = $aDate;
+    }
+
     $sDate1 = json_encode($aNewDate);
     $fp = fopen($broadcast, "w+"); //w是寫入模式,檔案不存在則建立檔案寫入。
     fwrite($fp, $sDate1);
@@ -51,9 +58,11 @@ if ($type == "read") {
     $history = 'history.txt';
     $str = file_get_contents($history);
     $aDate2 = json_decode($str, true);
-    $aDate2[] = $saveDate;
-    if (count($aDate2) > 5) {
-        array_splice($aDate2,0,1);
+    if($admin=="admin"){
+        $aDate2[] = $saveDate;
+        if (count($aDate2) > 5) {
+            array_splice($aDate2,0,1);
+        }
     }
     $sDate2 = json_encode($aDate2);
     $fp = fopen($history, "w+"); //w是寫入模式,檔案不存在則建立檔案寫入。
